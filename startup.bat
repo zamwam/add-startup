e17d12ec75085e6678a9be03d5eacd04340597d0
zamwam <159726433+zamwam@users.noreply.github.com>
2026-07-14 17:40:35 -0400

    Create startup.bat

---
+++ startup.bat
@@ -0,0 +1,25 @@
+@echo off
+echo Adding program to Startup...
+
+:: Path to the program you want to run on startup
+set "program=C:\Users\{changeme}\Music\startupscript.bat"
+
+:: Your personal Startup folder (no admin needed)
+set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+:: Shortcut name
+set "shortcut=%startup%\LIVE Earnings.lnk"
+
+echo Creating shortcut...
+
+powershell -command ^
+ "$ws = New-Object -ComObject WScript.Shell;" ^
+ "$s = $ws.CreateShortcut('%shortcut%');" ^
+ "$s.TargetPath = '%program%';" ^
+ "$s.Save()"
+
+echo Done.
+echo Shortcut created at:
+echo %shortcut%
+
+pause
